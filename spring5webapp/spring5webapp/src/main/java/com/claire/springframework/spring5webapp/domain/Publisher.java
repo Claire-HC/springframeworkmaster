11acf3b550e3bf6eeaae079e0fb9675f81ed5815
Claire Chen <hche0040@student.monash.edu>
2020-11-13 15:29:04 +1100

    Adding Publisher (POJO)

---
+++ spring5webapp/spring5webapp/src/main/java/com/claire/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,68 @@
+package com.claire.springframework.spring5webapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by claire on 13/11/20
+ */
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String address;
+
+    public Publisher(){
+
+    }
+
+    public Publisher(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+
+        if(obj == null || getClass() != obj.getClass())
+            return false;
+
+        Publisher publisher = (Publisher) obj;
+        return id != null ? id.equals(publisher.id) : publisher.id == null;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher { " + "id = " + id +
+                ", name = " + name +
+                ", address = " + address + " }";
+    }
+}
