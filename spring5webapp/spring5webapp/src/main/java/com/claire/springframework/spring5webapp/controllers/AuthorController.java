5b5df0c70bc3033137d002f39b444718ef229e78
Claire Chen <hche0040@student.monash.edu>
2020-11-18 22:26:03 +1100

    Adding author controller

---
+++ spring5webapp/spring5webapp/src/main/java/com/claire/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,27 @@
+package com.claire.springframework.spring5webapp.controllers;
+
+import com.claire.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by claire on 18/11/20
+ */
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthor(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
